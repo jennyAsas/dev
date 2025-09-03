72466ef9acd970e64a368885ec46e240e227d169
Jenny Asas <jenny@example.com>
2025-09-03 14:55:42 +0800

    Clear old website files

--- src/app/projects/projects.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-projects',
-  standalone: true,
-  templateUrl: './projects.html',
-  styleUrls: ['./projects.css']
-})
-export class Projects {}
